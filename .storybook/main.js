784269f4a16c9f4ef3420c2f63d36f285d44cd6f
trailblazing-dev <thong@trailblazing.dev>
2021-11-17 01:13:17 -0800

    new library added along with storybook

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "stories": ['../projects/ngx-ui/src/lib/**/*.stories.ts'],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
---
+++ projects/ngx-ui/README.md
@@ -0,0 +1,3 @@
+# ngx-UI
+
+An Angular library of UI components from Trailblazing Developers.
